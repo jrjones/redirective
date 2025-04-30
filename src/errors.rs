593ba0cf466f0785142fa7835c90f473c8f64de5
Joseph R. Jones <jrj@jrj.org>
2025-04-29 21:50:17 -0400

    Update errors.rs with copyright

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,4 @@
+// (C) Copyright 2025, Joseph R. Jones - https://jrj.org - Licensed under MIT License
 //! errors module: central error type for redirective service.
 
 /// A generic error type for the application.
